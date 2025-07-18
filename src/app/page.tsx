cca22040ef22d4f3b62f01a982f598445d3dacae
dimaspryga <dimaspryga9@gmail.com>
2025-07-19 05:01:55 +0700

    feat: add dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Input/>
-      <Button>Hello</Button>
-      <DarkmodeToggle />
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Dimas Prayoga Akbar</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
